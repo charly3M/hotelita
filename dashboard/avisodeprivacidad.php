936ff88aa54b829d14e90c8513678a6bf400491e
unknown <carlospadilla464@gmail.com>
2022-06-01 16:24:38 -0500

    Se agrega el proyecto

---
+++ dashboard/avisodeprivacidad.php
@@ -0,0 +1,4 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<?php require_once "vistas/parte_inferior.php"?>
---
+++ dashboard/ganancias.php
@@ -0,0 +1,4 @@
+<?php require_once "vistas/adminpartesup.php"?>
+<img src="imagenes/graph.png" alt="">
+<?php require_once "vistas/parte_inferior.php"?>
+
